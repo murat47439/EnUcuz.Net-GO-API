4ac77356361c506a0e8806ea6a9ff4f2373bd0ef
Murat Turan <murat47439@gmail.com>
2025-09-08 22:39:40 +0300

    FİX 12

--- repo/categoriesRepo.go
+++ repo/categoriesRepo.go
@@ -48,9 +48,9 @@ func (cr *CategoriesRepo) AddCategory(data *models.Category) (*models.Category, 
 		return nil, fmt.Errorf("Category does exists")
 	}
 
-	query := `INSERT INTO categories(name,created_at) VALUES ($1, NOW()) RETURNING id`
+	query := `INSERT INTO categories(name,parent_id,created_at) VALUES ($1, $2 ,NOW()) RETURNING id`
 	var id int
-	err = tx.QueryRow(query, data.Name).Scan(&id)
+	err = tx.QueryRow(query, data.ParentID, data.Name).Scan(&id)
 
 	data.ID = id
 
