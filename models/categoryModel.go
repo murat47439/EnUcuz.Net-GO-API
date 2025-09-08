2c976fb717a0fc7f0a8038f9a364147f6d0287f8
Murat Turan <murat47439@gmail.com>
2025-09-08 22:49:21 +0300

    FİX 13

--- models/categoryModel.go
+++ models/categoryModel.go
@@ -8,7 +8,7 @@ import (
 type Category struct {
 	ID          int           `json:"id" db:"id"`
 	Name        string        `json:"name" db:"name"`
-	ParentID    *int          `json:"parentId,omitempty" db:"parent_id"`
+	ParentID    *int          `json:"parent_id,omitempty" db:"parent_id"`
 	SubCategory []Category    `json:"subCategories,omitempty"`
 	DeletedAt   *sql.NullTime `json:"-" db:"deleted_at"`
 	CreatedAt   time.Time     `json:"created_at,omitempty" db:"created_at"`
