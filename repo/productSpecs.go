0a1ffed58583c7cc3faae8132102f2e99f6650b7
Murat Turan <murat47439@gmail.com>
2025-09-15 02:24:00 +0300

    fix3

--- repo/productSpecs.go
+++ repo/productSpecs.go
@@ -118,7 +118,7 @@ func (psr *ProductSpecsRepo) getBattery(prodid int) (models.Battery, error) {
 	}
 	query := `SELECT technology,capacity FROM battery WHERE product_id = $1`
 
-	err := psr.db.QueryRowx(query, prodid).Scan(&battery.Technology, battery.Capacity)
+	err := psr.db.QueryRowx(query, prodid).Scan(&battery)
 	if err != nil {
 		return models.Battery{}, err
 	}
